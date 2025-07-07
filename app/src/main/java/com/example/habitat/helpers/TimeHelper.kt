3260d04c92f2b1444a52c7a617e1e3b6c8d9ff73
ro-vezur <authenttesting@gmail.com>
2025-07-07 23:35:07 +0300

    feat: helpers

--- app/src/main/java/com/example/habitat/helpers/TimeHelper.kt
+++ app/src/main/java/com/example/habitat/helpers/TimeHelper.kt
@@ -1,9 +1,14 @@
 package com.example.habitat.helpers
 
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
 import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
+import java.util.Date
 import java.util.Locale
 
 object TimeHelper {
@@ -51,10 +56,18 @@ object TimeHelper {
         return date.year
     }
 
-    fun getDayBoundsInMillis(date: LocalDate): Pair<Long, Long> {
-        val startOfDay = date.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
-        val endOfDay = date.plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli() - 1
-        return startOfDay to endOfDay
+    fun getWeekBoundsFromMillis(dateMillis: Long): Pair<Long, Long> {
+        val zone = ZoneId.systemDefault()
+        val date = Instant.ofEpochMilli(dateMillis).atZone(zone).toLocalDate()
+
+        val startOfWeek = date.with(DayOfWeek.MONDAY).atStartOfDay(zone).toInstant().toEpochMilli()
+        val endOfWeek = date.with(DayOfWeek.SUNDAY)
+            .atTime(LocalTime.MAX)
+            .atZone(zone)
+            .toInstant()
+            .toEpochMilli()
+
+        return startOfWeek to endOfWeek
     }
 
     fun createLocalDateOfTimestamp(millis: Long): LocalDate {
@@ -63,6 +76,32 @@ object TimeHelper {
             getMonthNumberFromMillis(millis),
             getDayOfMonthNumberFromMillis(millis)
         )
+    }
+
+    fun formatDateFromMillis(millis: Long, dateFormat: String): String {
+        val sdf = SimpleDateFormat(dateFormat, Locale.getDefault())
+        return sdf.format(Date(millis))
+    }
+
+    fun formatDateFromHoursAndMinutes(hours: Int, minutes: Int): String {
+        val local = LocalTime.of(hours,minutes)
+        return local.format(DateTimeFormatter.ofPattern(DateFormats.HH_MM))
+    }
+
+    fun convertHoursAndMinutesIntoMillis(hours: Int,minutes: Int): Long {
+        return (hours * 60 * 60 * 1000L) + (minutes * 60 * 1000L)
+    }
+
+    fun getStartOfDayMillis(timestamp: Long): Long {
+        val zone = ZoneId.systemDefault()
+        val localDate = Instant.ofEpochMilli(timestamp).atZone(zone).toLocalDate()
+        return localDate.atStartOfDay(zone).toInstant().toEpochMilli()
+    }
+
+    fun getCurrentDayOfWeekObject(): DayOfWeek = LocalDate.now().dayOfWeek
+
+    object DateFormats {
+        const val HH_MM = "HH:mm"
     }
 
 }

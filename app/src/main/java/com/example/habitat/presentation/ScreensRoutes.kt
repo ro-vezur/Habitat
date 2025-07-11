c59a2be19a538a4b9665d20fc927133181fa6bad
ro-vezur <authenttesting@gmail.com>
2025-07-11 23:34:58 +0300

    feat: screen routes

--- app/src/main/java/com/example/habitat/presentation/ScreensRoutes.kt
+++ app/src/main/java/com/example/habitat/presentation/ScreensRoutes.kt
@@ -1,6 +1,11 @@
 package com.example.habitat.presentation
 
+import com.example.habitat.domain.entities.Habit
+import com.example.habitat.helpers.serializations.encodeJsonObject
+import kotlinx.serialization.Serializable
 
+
+@Serializable
 sealed class ScreensRoutes(val route: String) {
     data object SplashScreen: ScreensRoutes(route = "SplashScreenRoute")
 
@@ -13,4 +18,11 @@ sealed class ScreensRoutes(val route: String) {
     data object AddHabit: ScreensRoutes(route = "AddHabitScreenRoute")
 
     data object Statistics: ScreensRoutes(route = "StatisticsScreenRoute")
+
+    data object DetailedHabit: ScreensRoutes(route = "${Home.route}/{habit}") {
+        fun createRoute(habit: Habit): String {
+
+            return route.replace("{habit}", encodeJsonObject(habit))
+        }
+    }
 }
